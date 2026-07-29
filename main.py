13ee7adeb225b00afbd126fe6766ee75490b64ba
Harry Vicaradge (BSc Theor Ph + App Maths FT) <hxv413@student.bham.ac.uk>
2026-07-29 22:34:47 +0100

    logic change

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ N = 2
 x = solver_module(N)
 
 def u_r(r, mew):
-    i = np.arange(5 * N + 3)
-    L_vals = np.polynomial.legendre.legvander(mew, N)[0]
+    n = np.arange(1, N + 1)
+
+    L_vals = np.polynomial.legendre.legvander(mew, N)[0][1, N + 1]
+
+    factor = (2 * n + 1) * L_vals
+
     u_r_coeff = np.zeros(5 * N + 3)
-    u_r_coeff[::5] = (2 * (i + 1) + 1) *  L_vals[i // 5] * r**((i + 1)+3)
-    u_r_coeff[1::5] = (2 * (i + 1) + 1) *  L_vals[i // 5] * r**((i + 1)+1)
-    u_r_coeff[2::5] = (2 * (i + 1) + 1) *  L_vals[i // 5] * r**(2-(i + 1))
-    u_r_coeff[3::5] = (2 * (i + 1) + 1) *  L_vals[i // 5] * r**(-(i + 1))
+
+    u_r_coeff[0 : 5 * N : 5] = factor * r ** (n + 4)  
+    u_r_coeff[1 : 5 * N : 5] = factor * r ** (n + 2)
+    u_r_coeff[2 : 5 * N : 5] = factor * r ** (1 - n)  
+    u_r_coeff[3 : 5 * N : 5] = factor * r ** (-(n + 1))
     u_r_coeff[-3:] = 0
+
     return np.dot(u_r_coeff, x)

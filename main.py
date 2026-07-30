84967706689bf74bc3a2be655aa497a330f78627
Harry Vicaradge (BSc Theor Ph + App Maths FT) <hxv413@student.bham.ac.uk>
2026-07-30 17:09:59 +0100

    finished the u_r function, untested, started on u_theta and plot

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import solver_module
 N = 2
 x = solver_module(N)
 
-def u_r(r, mew):
+def u_r(r, mew):    # de-dimensionalised equations. instead of inputing r = R for example, input r' = 1
+
     n = np.arange(1, N + 1)
-
     L_vals = np.polynomial.legendre.legvander(mew, N)[0][1, N + 1]
-
     factor = (2 * n + 1) * L_vals
 
     u_r_coeff = np.zeros(5 * N + 3)
@@ -19,4 +18,22 @@ def u_r(r, mew):
     u_r_coeff[3 : 5 * N : 5] = factor * r ** (-(n + 1))
     u_r_coeff[-3:] = 0
 
-    return np.dot(u_r_coeff, x)
+    u_r_i = np.dot(u_r_coeff, x)
+
+    u_r_coeff = np.zeros(5 * N + 3)
+    u_r_coeff[-2] = -1 * L_vals[1]
+    u_r_coeff[4 : 5 * N : 5] = factor *(r**(-n) - r**((-n -2))) 
+
+    u_r_o = np.dot(u_r_coeff, x) 
+
+    return u_r_o, u_r_i
+
+
+def u_theta(r,mew):
+
+    return u_theta_o, u_theta_i
+
+
+def main():
+        
+    return 0
